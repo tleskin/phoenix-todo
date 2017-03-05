ba47bd188d1f207823c7ac128f7e2f1720ab91f7
Tom Leskin <tleskin@gmail.com>
2017-03-05 15:24:32 -0500

    phoenix-todo

---
+++ web/models/user.ex
@@ -0,0 +1,20 @@
+defmodule Todo.User do
+  use Todo.Web, :model
+
+  schema "users" do
+    field :email, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, ~w(email), [])
+    |> validate_format(:email, ~r/@/)
+  end
+end
